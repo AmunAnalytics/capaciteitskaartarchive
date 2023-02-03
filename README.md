219b2b3b1fdea71293596e59a5341281e3f45ffc
Frank Boerman <frank@fboerman.nl>
2023-02-03 21:26:56 +0100

    add pull script and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Archief capaciteitskaart netbeheer Nederland
+
+Deze repo bevat een historisch archief van alle wijzigingen van de capaciteitskaart van Netbeheer Nederland zoals gedownload van https://capaciteitskaart.netbeheernederland.nl/
+
+De data wordt automatisch aangevuld zodra beschikbaar.
+
+## Disclaimer
+Dit is een onofficieel initiatief en heeft dus geen enkele officiele connectie met netbeheer Nederland.
---
+++ pull.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [[ ! -e last_updated.txt ]]; then
+	touch last_updated.txt
+fi
+
+curl -s https://capaciteitskaart.netbeheernederland.nl/ | grep Bijgewerkt | grep -E -o '[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2}' > /tmp/netbeheerkaart_last_updated.txt
+site_online=$?
+
+if [[ $site_online == 0 ]]; then
+	timestamp_diff="$(diff /tmp/netbeheerkaart_last_updated.txt last_updated.txt)"
+
+	if [[ "0" != "${#timestamp_diff}" ]]; then
+		curl -s https://capaciteitskaart.netbeheernederland.nl/dashboard/download > latest.csv
+		mv /tmp/netbeheerkaart_last_updated.txt last_updated.txt
+		timestamp="$(cat last_updated.txt)"
+		cp latest.csv archive/"${timestamp// /_}".csv
+
+		git add *.csv
+		git add archive/*.csv
+		git commit -m "beep boop automated update" --author="Amun Analytics Bot <frank@amunanalytics.eu>"
+		#git push
+	fi
+fi
